2f3615d50ae3375dac041d90f888a489023a5ef4
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-10-27 13:33:13 +0100

    Update Toolbar.tsx

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -3,15 +3,15 @@ import React from "react"
 type Props = {
   onAddRow: () => void
   onDeleteRow: () => void
-  onExportCsv: () => void
+  onPrint: () => void
 }
 
-const Toolbar: React.FC<Props> = ({ onAddRow, onDeleteRow, onExportCsv }) => {
+const Toolbar: React.FC<Props> = ({ onAddRow, onDeleteRow, onPrint }) => {
   return (
     <div className="toolbar">
       <button className="toolbtn ok" onClick={onAddRow}>➕ Ny rad</button>
       <button className="toolbtn danger" onClick={onDeleteRow}>🗑️ Slett valgt rad</button>
-      <button className="toolbtn" onClick={onExportCsv}>⤓ Eksporter CSV</button>
+      <button className="toolbtn" onClick={onPrint}>🖨️ Skriv ut / PDF</button>
     </div>
   )
 }
