75dea3390ee68cf7744fa314e12e7905ee079813
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-10-28 14:08:50 +0100

    Delete src directory

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-/* ==== [BLOCK: Imports] BEGIN ==== */
-import React from "react"
-import { createRoot } from "react-dom/client"
-import App from "./App"
-import "./index.css"
-/* ==== [BLOCK: Imports] END ==== */
-
-createRoot(document.getElementById("root")!).render(<App />)
