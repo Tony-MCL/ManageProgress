1f41f3a366349569b80d544dc8f3c4f603c3eef7
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-10-29 09:36:02 +0100

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+/* ==== [BLOCK: Imports] BEGIN ==== */
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+/* ==== [BLOCK: Imports] END ==== */
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
