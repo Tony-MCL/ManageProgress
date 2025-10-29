4ef6694019eb91fc43059ba422966fab4f0cfac0
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-10-29 09:14:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/MangeProgress/",         // ← VIKTIG for GitHub Pages (repo-navn)
   server: { port: 5173 },
   build: { outDir: "dist" }
 })
