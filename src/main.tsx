155375ca66f93b7c4a1f1bccde80f347ec5c71f9
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-03 11:47:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import { applyTheme } from "./core/theme";
-// applyTheme("light" | "dark" | "auto");
-/* ==== [BLOCK: Imports] END ==== */
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
